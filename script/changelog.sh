0e71b0ab5b85dcbdcf00737e0d7527940babcdf4
anstk <anstk@envidera.com>
2024-08-07 20:45:02 -0300

    update

---
+++ script/changelog.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Encontrar o commit anterior ao último
+previous_commit=$(git rev-parse HEAD~1)
+
+# Encontrar o caminho do arquivo CHANGELOG.md no último commit
+current_file_path=$(git ls-tree -r HEAD --name-only | grep -i 'CHANGELOG.md')
+
+# Encontrar o caminho do arquivo CHANGELOG.md no commit anterior
+previous_file_path=$(git ls-tree -r $previous_commit --name-only | grep -i 'CHANGELOG.md')
+
+# Verificar se o arquivo existe no último commit
+if [ -n "$current_file_path" ]; then
+    # Verificar se o arquivo também existe no commit anterior
+    if [ -n "$previous_file_path" ]; then
+        # Comparar o arquivo CHANGELOG.md entre o último commit e o commit anterior
+        git diff --unified=0 $previous_commit HEAD -- "$current_file_path" | awk '/^\+|^-/' | sed -e 's/^[+-]//' | sed '1,2d'  > auto_changelog.txt
+    else
+        echo "Arquivo CHANGELOG.md não encontrado no commit anterior."
+    fi
+else
+    echo "Arquivo CHANGELOG.md não encontrado no último commit."
+fi
+
