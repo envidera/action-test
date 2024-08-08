9b240785e0459261ff9d70c39fcd3b88f537955d
anstk <anstk@envidera.com>
2024-08-07 21:46:18 -0300

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,25 @@
 # Changelog
 
+
+## v0.1.765765-alpha
+- asdasd alpha
+- sdfsd 
+
+This action checks-out your repository under $GITHUB_WORKSPACE, so your workflow can access it.
+
+Only a single commit is fetched by default, for the ref/SHA that triggered the workflow. Set fetch-depth: 0 to fetch all history for all branches and tags. Refer here to learn which commit $GITHUB_SHA points to for different events.
+
+The auth token is persisted in the local git config. This enables your scripts to run authenticated git commands. The token is removed during post-job cleanup. Set persist-credentials: false to opt-out.
+
+When Git 2.18 or higher is not in your PATH, falls back to the REST API to download the files.
+
 ## v4
 - asdasd4
 - sdfsd4
+Vasda ça çsd
+çksd ças
+
+asçkas
 
 
 ## v3
--- script/changelog.sh
+++ script/changelog.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
-set -e
 
-# Verificar se há commits suficientes
-commit_count=$(git rev-list --count HEAD)
-if [ "$commit_count" -lt 2 ]; then
-    echo "There are not enough commits to compare." > auto_changelog.txt
-    exit 0
-fi
+# Define o arquivo do changelog
+CHANGELOG_FILE="CHANGELOG.md"
 
-# Encontrar o commit anterior ao último
-previous_commit=$(git rev-parse HEAD~1)
-
-# Encontrar o caminho do arquivo CHANGELOG.md no último commit
-current_file_path=$(git ls-tree -r HEAD --name-only | grep -i 'CHANGELOG.md')
-
-# Encontrar o caminho do arquivo CHANGELOG.md no commit anterior
-previous_file_path=$(git ls-tree -r $previous_commit --name-only | grep -i 'CHANGELOG.md')
-
-# Verificar se o arquivo existe no último commit
-if [ -n "$current_file_path" ]; then
-    # Verificar se o arquivo também existe no commit anterior
-    if [ -n "$previous_file_path" ]; then
-        # Comparar o arquivo CHANGELOG.md entre o último commit e o commit anterior
-        git diff --unified=0 $previous_commit HEAD -- "$current_file_path" | awk '/^\+|^-/' | sed -e 's/^[+-]//' | sed '1,2d' > auto_changelog.txt
-    else
-        echo "CHANGELOG.md file not found in previous commit." > auto_changelog.txt
-    fi
-else
-    echo "CHANGELOG.md file not found in last commit." > auto_changelog.txt
-fi
+# Lê o conteúdo do changelog e extrai o último conteúdo
+awk '
+BEGIN { inside_section = 0; }
+/^## v[0-9]+/ {
+  if (inside_section == 1) {
+    exit;
+  }
+  inside_section = 1;
+  next;
+}
+inside_section == 1 {
+  print;
+}' "$CHANGELOG_FILE" > out
